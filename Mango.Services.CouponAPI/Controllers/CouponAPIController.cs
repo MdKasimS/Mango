893ba07c6a152af49aff45aa54abf21dcca25227
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-06 21:16:49 +0530

    Get methods - get all, get by id, get by code is added

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,48 +1,81 @@
-﻿using Mango.Services.CouponAPI.Data;
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Mango.Services.CouponAPI.Controllers
 {
     [Route("api/[controller]")]//-> this is shown in Swagger
-    //[ApiController]
+    [ApiController]
     public class CouponAPIController : ControllerBase
     {
         private readonly AppDbContext _db;
-        public CouponAPIController(AppDbContext db)
+        private ResponseDto _response;
+        private IMapper _mapper;
+
+        public CouponAPIController(AppDbContext db, IMapper mapper)
         {
             _db = db ;
+            _mapper = mapper;
+            _response = new ResponseDto();
         }
 
         [HttpGet]//-> internal name for URL
-        public object Get()
+        public ResponseDto Get()
         {
             try
             {
                 IEnumerable<Coupon> coupons = _db.Coupons.ToList();
-                return coupons;
+                _response.Result = _mapper.Map<IEnumerable<CouponDto>>(coupons);
             }
             catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Error retrieving data from the database: {ex.Message}");
+                _response.Result = StatusCodes.Status500InternalServerError;
+                _response.IsSucess = false;
+                _response.Message = ex.Message;
             }
+            return _response;
         }
 
         [HttpGet]//-> internal name for URL
         [Route("{id:int}")]
-        public object Get(int id)
+        public ResponseDto Get(int id)
         {
             try
             {
-                object coupon = _db.Coupons.First(c=>c.Id==id);
-                return coupon;
+                Coupon coupon = _db.Coupons.First(c=>c.Id==id);
+                _response.Result = _mapper.Map<CouponDto>(coupon);
             }
             catch (Exception ex)
             {
-                return StatusCode(StatusCodes.Status500InternalServerError, $"Error retrieving data from the database: {ex.Message}");
+                _response.Result = StatusCodes.Status500InternalServerError;
+                _response.IsSucess = false;
+                _response.Message = ex.Message;
             }
+                return _response;
         }
 
+        [HttpGet]//-> internal name for URL
+        [Route("GetByCode/{code}")]
+        public ResponseDto GetByCode(string code)
+        {
+            try
+            {
+                Coupon coupon = _db.Coupons.FirstOrDefault(c => c.CouponCode.ToLower() == code.ToLower());
+                _response.Result = _mapper.Map<CouponDto>(coupon);
+            }
+            catch (Exception ex)
+            {
+                _response.Result = StatusCodes.Status500InternalServerError;
+                _response.IsSucess = false;
+                _response.Message = ex.Message;
+            }
+            return _response;
+        }
+
+      
     }
 }
