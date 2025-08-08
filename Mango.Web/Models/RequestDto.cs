feabbabb34b4d8ca8cbf31690016f130decd927a
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-08 14:13:16 +0530

    save

---
+++ Mango.Web/Models/RequestDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Mango.Web.Models
+{
+    public class RequestDto
+    {
+        public string Url { get; set; }
+        public string ApiType { get; set; } = "GET"; 
+        public string AccessToken { get; set; }
+        public object? Data { get; set; } // Nullable to allow for no data in some requests
+    }
+}
