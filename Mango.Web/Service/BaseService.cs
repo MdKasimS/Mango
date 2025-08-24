9882ace78d640b809cfffaa95c3126f3acf06846
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-24 12:34:32 +0530

    I guess it will repeat. I did not pull before starting

--- Mango.Web/Service/BaseService.cs
+++ Mango.Web/Service/BaseService.cs
@@ -1,5 +1,8 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+using Newtonsoft.Json;
+using System.Net;
 
 namespace Mango.Web.Service
 {
@@ -7,13 +10,43 @@ namespace Mango.Web.Service
     {
         private readonly IHttpClientFactory _httpClientFactory;
 
-        public BaseService(IHttpClientFactory httpClientFactory) 
+        public BaseService(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
-        public Task<ResponseDto> SendAsync(RequestDto request)
+        public async Task<ResponseDto?> SendAsync(RequestDto requestDto)
         {
-            throw new NotImplementedException();
+            HttpClient client = _httpClientFactory.CreateClient("MangoAPI");
+            HttpRequestMessage message = new();
+            message.Headers.Add("Accept", "application/json");
+            //token
+
+            message.RequestUri = new Uri(requestDto.Url);
+            if (requestDto.Data != null)
+            {
+                message.Content = new StringContent(JsonConvert.SerializeObject(requestDto.Data)
+                                                    , System.Text.Encoding.UTF8, "application/json");
+            }
+
+            HttpResponseMessage? apiResponse = null;
+
+            if (requestDto.Data != null)
+            {
+                message.Method = requestDto.ApiType switch
+                {
+                    ApiType.POST => HttpMethod.Post,
+                    ApiType.PUT => HttpMethod.Put,
+                    ApiType.DELETE => HttpMethod.Delete,
+                    _ => HttpMethod.Get
+                };
+            }
+            apiResponse = await client.SendAsync(message);
+
+            switch(apiResponse.StatusCode)
+            {
+                case HttpStatusCode.NoContent:
+                    return new(){ IsSuccess = false, Message = "Not Found" };
+            }
         }
     }
 }
