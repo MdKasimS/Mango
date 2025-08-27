ca7a120a0e82c190068072460322cfc0a9927f2e
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-27 23:37:23 +0530

    added services in DI container. But did not get exactly why

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -7,10 +7,24 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpContextAccessor();
+
+/*This line adds the basic IHttpClientFactory and related services to the application's DI (Dependency Injection) container.
+It allows for creation and management of HttpClient instances with best practices (such as socket pooling and lifetime management),
+helping developers avoid common pitfalls like socket exhaustion.
+*/
 builder.Services.AddHttpClient();
+
+/* Registering a typed client for ICouponService with its implementation CouponService.
+ This allows for dependency injection of ICouponService wherever needed in the application.
+ The HttpClient instance provided to CouponService will be managed by the IHttpClientFactory,
+ ensuring proper configuration and lifecycle management. 
+ This is particularly useful for services that make HTTP calls, as it helps in reusing HttpClient instances efficiently.
+*/
+
 builder.Services.AddHttpClient<ICouponService, CouponService>();
 SD.CouponAPIBase = builder.Configuration["ServiceUrls: CouponAPI"];
 
+builder.Services.AddScoped<IBaseService, BaseService>();
 builder.Services.AddScoped<ICouponService, CouponService>();
 
 var app = builder.Build();
