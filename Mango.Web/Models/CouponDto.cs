99a813dadd532630bceeda4b78c0d938471acad6
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-08-31 03:36:02 +0530

    If you keep nullables in Dtos, validatoin scripts may not work accordingly

--- Mango.Web/Models/CouponDto.cs
+++ Mango.Web/Models/CouponDto.cs
@@ -2,9 +2,9 @@
 {
     public class CouponDto
     {
-        public int Id { get; set; }
-        public string? CouponCode { get; set; }
+        public int CouponId { get; set; }
+        public string CouponCode { get; set; }
         public double DiscountAmount { get; set; }
-        public double MinAmount { get; set; }
+        public int MinAmount { get; set; }
     }
 }
