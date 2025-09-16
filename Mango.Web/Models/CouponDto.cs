bdeaaada546d7debbb81570eb4d6a34abfed45f3
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-16 22:20:26 +0530

    Added AuthService required classes and interfaces. Registered in container

--- Mango.Web/Models/CouponDto.cs
+++ Mango.Web/Models/CouponDto.cs
@@ -4,6 +4,9 @@ namespace Mango.Web.Models
 {
     public class CouponDto
     {
+        //This is needed since Entity model has some other column name
+        //Response conatins exact table record matching column names that are in entity model
+        //and hence in table. Revealing potential details.
         [JsonProperty("id")]
         public int CouponId { get; set; }
         public string CouponCode { get; set; }
