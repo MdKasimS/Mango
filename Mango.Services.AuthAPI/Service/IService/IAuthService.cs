ef791afe12a7e87a571f3ead9a3e131cb0b423af
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-16 12:58:31 +0530

    Added methods needed.

---
+++ Mango.Services.AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+
+namespace Mango.Services.AuthAPI.Service.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
