59cfb664ca49ff1c3afa5f90f0251c991ae7d5dc
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-16 12:19:16 +0530

    IAuthService interface added

---
+++ Mango.Services.AuthAPI/Services/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Mango.Services.AuthAPI.Models.Dto;
+
+namespace Mango.Services.AuthAPI.Services.IService
+{
+    public interface IAuthService
+    {
+        Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+        public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    }
+}
