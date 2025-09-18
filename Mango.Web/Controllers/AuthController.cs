c8be39b1bbf4141436613ed4e88679c906e2d410
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-18 18:40:16 +0530

    Login & Register view is underwork

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -1,13 +1,15 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Mango.Web.Controllers
 {
     public class AuthController : Controller
     {
         private readonly IAuthService _authService;
-        public AuthController(IAuthService authService) 
+        public AuthController(IAuthService authService)
         {
             _authService = authService;
         }
@@ -22,12 +24,20 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem{Text=SD.RoleAdmin, Value=SD.RoleAdmin},
+                new SelectListItem{Text=SD.RoleCustomer, Value=SD.RoleCustomer }
+            };
+
+            ViewBag.RoleList = roleList;
             return View();
         }
 
         [HttpGet]
         public IActionResult Logout()
         {
+            ViewBag.RoleList = "Check";
             return View();
         }
     }
--- Mango.Web/Utility/SD.cs
+++ Mango.Web/Utility/SD.cs
@@ -4,6 +4,8 @@
     {
         public static string CouponAPIBase { get; set; }
         public static string AuthAPIBase { get; set; }
+        public static string RoleAdmin = "ADMIN";
+        public static string RoleCustomer = "CUSTOMER";
         public enum ApiType
         {
             GET,
