03efa3716306de869e2f47a9ce24b912e5067083
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-26 19:17:29 +0530

    Added TODO in Coupon entity

--- Mango.Services.CouponAPI/Models/Coupon.cs
+++ Mango.Services.CouponAPI/Models/Coupon.cs
@@ -14,14 +14,15 @@ namespace Mango.Services.CouponAPI.Models
         public double DiscountAmount { get; set; }
         public double MinAmount { get; set; }
 
+        //TODO: Advanced versions needs these
         //public DateTime CreatedDate { get; set; }
         //public DateTime ExpiryDate { get; set; }
         //public bool IsActive { get; set; }
         //public string? CreatedBy { get; set; }
         //public string? UpdatedBy { get; set; }
         //public DateTime UpdatedDate { get; set; }
-        //public string? Description { get; set; }
         //public string? CouponType { get; set; } // e.g., "Percentage", "Amount"
+        //public string? Description { get; set; }
         //public string? ProductName { get; set; } // Optional, if the coupon is specific to a product
         //public string? ProductCategory { get; set; } // Optional, if the coupon is specific to a category
         //public string? ProductDescription { get; set; }
