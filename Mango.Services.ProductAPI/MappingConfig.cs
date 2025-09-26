e34f24c6810f7e38961854fabf105f5543d399e1
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-26 21:00:48 +0530

    Added AutoMapper logic

---
+++ Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var loggerFactory = LoggerFactory.Create(builder => {
+                builder.AddConsole();
+            });
+
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            }, loggerFactory);
+            return mappingConfig;
+        }
+    }
+}
