7d117afd294aad1c27f5b95225625ab08d3cf37d
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-27 14:34:06 +0530

    HomeController is serving now products. Need to modify authorize access

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,22 +1,38 @@
 using Mango.Web.Models;
+using Mango.Web.Service.IService;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace Mango.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IProductService _productService;
+        private readonly ResponseDto _response;
+        public HomeController(IProductService productService)
         {
-            _logger = logger;
+            _productService = productService;
+            _response = new ResponseDto();
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<ProductDto>? list = new();
+            ResponseDto? response = await _productService.GetAllProductsAsync();
+            if (response != null && response.IsSuccess)
+            {
+                //TODO: De Serialization is not working correctly. It did not mapped Ids for CouponId
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            //This View() is connected to HomeIndex.cshtml
+            return View(list);
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
         public IActionResult Privacy()
         {
