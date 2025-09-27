dc145b5963e20b6a0aa2dc02186d46b77474ddbf
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-27 19:22:19 +0530

    Added AppDbContext for database without initial database seeding.

---
+++ Mango.Services.ShoppingCartAPI/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Mango.Services.ShoppingCartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ShoppingCartAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<CartHeader> CartHeaders { get; set; } = null!;
+        public DbSet<CartDetails> CartDetails { get; set; } = null!;
+
+        /// We can use this method to configure the model further if needed.
+        /// For now, it's commented out as we don't have additional configurations.
+        /// Let's keep it here for future reference.
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder){}
+        
+}
+}
