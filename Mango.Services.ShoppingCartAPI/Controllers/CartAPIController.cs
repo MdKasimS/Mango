e7b9fd82be72e5ccbd28b8b97f8bac2189bc817d
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-27 22:47:45 +0530

    Auto Mapper bug. Halting development

--- Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
+++ Mango.Services.ShoppingCartAPI/Controllers/CartAPIController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Mango.Services.ShoppingCartAPI.Data;
+using Mango.Services.ShoppingCartAPI.Models;
 using Mango.Services.ShoppingCartAPI.Models.Dto;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Mango.Services.ShoppingCartAPI.Controllers
 {
@@ -13,9 +15,9 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         private readonly AppDbContext _db;
         private readonly IMapper _mapper;
         private readonly ResponseDto response;
-        public CartAPIController(AppDbContext _db, IMapper mapper)
+        public CartAPIController(AppDbContext db, IMapper mapper)
         {
-            _db = _db;
+            _db = db;
             _mapper = mapper;
             response = new ResponseDto();
         }
@@ -24,7 +26,71 @@ namespace Mango.Services.ShoppingCartAPI.Controllers
         [HttpPost("CartUpsert")]
         public async Task<ResponseDto> CartUpsert(CartDto cartDto)
         {
-            
+            try
+            {
+                var cartHeaderFromDb = await _db.CartHeaders.FirstOrDefaultAsync(u => u.UserId == cartDto.CartHeader.UserId);
+                if (cartHeaderFromDb == null)
+                {
+                    // Create new cart header and details
+                    CartHeader cartHeader = _mapper.Map<CartHeader>(cartDto.CartHeader);
+                    _db.CartHeaders.Add(cartHeader);
+                    _db.SaveChanges();
+                    //_db.SaveChangesAsync();
+
+                    cartDto.CartDetails.First().CartHeaderId = cartHeader.Id;
+                    CartDetails cartDetails = _mapper.Map<CartDetails>(cartDto.CartDetails.First());
+                    _db.CartDetails.Add(cartDetails);
+                    _db.SaveChanges();
+                    //_db.SaveChangesAsync();
+
+                    foreach (var detail in cartDto.CartDetails)
+                    {
+                        detail.CartHeaderId = cartDto.CartHeader.Id;
+                    }
+                }
+                else
+                {
+                    // If cart header exists, check for cart details
+                    // If details for the product do not exist, create them
+
+                    //TODO: Review this part AsNoTracking() how it can benfit us here
+
+                    var cartDetailsFromDb = await _db.CartDetails.FirstOrDefaultAsync(
+                        u => u.ProductId == cartDto.CartDetails.First().ProductId 
+                        && u.CartHeaderId == cartHeaderFromDb.Id);
+
+                    if (cartDetailsFromDb == null)
+                    {
+                        //create cart details
+                        cartDto.CartDetails.First().CartHeaderId = cartHeaderFromDb.Id;
+                        CartDetails cartDetails = _mapper.Map<CartDetails>(cartDto.CartDetails.First());
+                        _db.CartDetails.Add(cartDetails);
+                        _db.SaveChanges();
+                        //_db.SaveChangesAsync();
+                    }
+                    else
+                    {
+                        //update the count / cart details
+                        cartDto.CartDetails.First().Count += cartDetailsFromDb.Count;
+
+                        //TODO: Review if we need to map all properties or just the count
+                        cartDto.CartDetails.First().CartHeaderId = cartDetailsFromDb.CartHeaderId;
+                        cartDto.CartDetails.First().Id = cartDetailsFromDb.Id;
+
+                        CartDetails cartDetails = _mapper.Map<CartDetails>(cartDto.CartDetails.First());
+                        _db.CartDetails.Update(cartDetails);
+                        _db.SaveChanges();
+                        //_db.SaveChangesAsync();
+                    }
+                }
+                response.Result = cartDto;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccess = false;
+                response.Message = ex.Message;
+            }
+            return response;
         }
 
     }
--- Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -5,7 +5,7 @@ namespace Mango.Services.ShoppingCartAPI.Models.Dto
 {
     public class CartHeaderDto
     {
-        public int CartHeaderId { get; set; }
+        public int Id { get; set; }
         public string? UserId { get; set; }
         public string? CouponCode { get; set; }
         public double Discount { get; set; }
