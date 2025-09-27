618cdac20b6c8b8e959484762e83c7aae19d50f2
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-27 19:22:49 +0530

    Added MappingConfig

---
+++ Mango.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Mango.Services.ShoppingCartAPI.Models;
+using Mango.Services.ShoppingCartAPI.Models.Dto;
+
+namespace Mango.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var loggerFactory = LoggerFactory.Create(builder => {
+                builder.AddConsole();
+            });
+
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+                //config.CreateMap<Product, ProductDto>();
+            }, loggerFactory);
+            return mappingConfig;
+        }
+    }
+}
