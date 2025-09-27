edc9f1e0d413bbc38c82e4729cc669cc4f3656bf
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-27 08:58:43 +0530

    IProductService & ProductService created

---
+++ Mango.Web/Service/IService/IProductService.cs
@@ -0,0 +1,15 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Service.IService
+{
+    public interface IProductService
+    {
+        Task<ResponseDto?> GetAllProductsAsync();
+        Task<ResponseDto?> GetProductByIdAsync(int id);
+        Task<ResponseDto?> GetProductAsync(string productName);
+        Task<ResponseDto?> GetProductByCategoryAsync(string productCategory);
+        Task<ResponseDto?> UpdateProductAsync(ProductDto productDto);
+        Task<ResponseDto?> CreateProductAsync(ProductDto productDto);
+        Task<ResponseDto?> DeleteProductAsync(int id);
+    }
+}
---
+++ Mango.Web/Service/ProductService.cs
@@ -0,0 +1,81 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IBaseService _baseService;
+        public ProductService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> GetAllProductsAsync()
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/product"
+            });
+        }
+
+        public async Task<ResponseDto?> GetProductByIdAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/product/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> GetProductAsync(string productName)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/product/GetByName/" + productName
+            });
+        }
+
+        public async Task<ResponseDto?> GetProductByCategoryAsync(string productCategory)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.ProductAPIBase + "/api/product/GetByCategory/" + productCategory
+            });
+        }
+
+        public async Task<ResponseDto?> CreateProductAsync(ProductDto productDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Url = SD.ProductAPIBase + "/api/product",
+                Data = productDto
+            });
+        }
+
+        public async Task<ResponseDto?> UpdateProductAsync(ProductDto productDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.PUT,
+                Url = SD.ProductAPIBase + "/api/product",
+                Data = productDto
+            });
+        }
+        public async Task<ResponseDto?> DeleteProductAsync(int id)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Data = id,
+                Url = SD.ProductAPIBase + "/api/product/" + id
+            });
+        }
+
+    }
+}
