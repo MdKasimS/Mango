b7cea395da8c1ad7ff722fc04701ba4a444ead6f
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-27 15:41:48 +0530

    Product details page is working perfectly for authorized user.

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
@@ -17,6 +18,7 @@ namespace Mango.Web.Controllers
         }
         public async Task<IActionResult> Index()
         {
+            //TODO: Make it symmetric layout. Variable length of Description makes it asymmetric
             List<ProductDto>? list = new();
             ResponseDto? response = await _productService.GetAllProductsAsync();
             if (response != null && response.IsSuccess)
@@ -31,6 +33,26 @@ namespace Mango.Web.Controllers
 
             //This View() is connected to HomeIndex.cshtml
             return View(list);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> ProductDetails(int productId)
+        {
+            //TODO: Make it symmetric layout. Variable length of Description makes it asymmetric
+            ProductDto? model = new();
+            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+            if (response != null && response.IsSuccess)
+            {
+                //TODO: De Serialization is not working correctly. It did not mapped Ids for CouponId
+                model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            //This View() is connected to HomeIndex.cshtml
+            return View(model);
         }
 
 
