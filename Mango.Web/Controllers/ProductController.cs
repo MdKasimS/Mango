a30be078db237744073d75fccbe9d14b08ca01c1
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-27 09:41:03 +0530

    Product View is ready - added to navbar, index view is ready, controller for getting all products is ready in controller

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ResponseDto _response;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+            _response = new ResponseDto();
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto>? list = new();
+            ResponseDto? response = await _productService.GetAllProductsAsync();
+            if (response != null && response.IsSuccess)
+            {
+                //TODO: De Serialization is not working correctly. It did not mapped Ids for CouponId
+                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+
+            //This View() is connected to ProductIndex.cshtml
+            return View(list);
+        }
+        
+
+    }
+}
