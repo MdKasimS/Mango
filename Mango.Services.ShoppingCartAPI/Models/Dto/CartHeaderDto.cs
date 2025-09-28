e2b1778861bf9e7697a250893943a1aeb277dd47
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-28 17:10:58 +0530

    Id changed to CartHeaderId

--- Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
+++ Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -2,7 +2,7 @@
 {
     public class CartHeaderDto
     {
-        public int Id { get; set; }
+        public int CartHeaderId { get; set; }
         public string? UserId { get; set; }
         public string? CouponCode { get; set; }
         public double Discount { get; set; }
