5e05c7e2afd83237575f1aef0cd08ff4f440da5e
KasimSache <45384577+MdKasimS@users.noreply.github.com>
2025-09-29 16:19:34 +0530

    Working on controller and views for shopping cart.

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -1,5 +1,10 @@
-﻿using Mango.Web.Service.IService;
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.IdentityModel.Tokens.Jwt;
+using System.Threading.Tasks;
 
 namespace Mango.Web.Controllers
 {
@@ -11,9 +16,26 @@ namespace Mango.Web.Controllers
             _cartService = cartService;
         }
 
-        public IActionResult Index()
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
         {
-            return View();
+            return View(await LoadCartDtoBasedOnLoggedInUser());
+        }
+
+        private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
+        {
+            //TODO: Why this is working? How user was accessed?
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?
+                                    .FirstOrDefault()?.Value;
+
+            var response =  await _cartService.GetCartByUserIdAsync(userId);    
+            
+            if(response!=null)
+            {
+                CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result));
+                return cartDto;
+            }
+            return new CartDto();
         }
     }
 }
