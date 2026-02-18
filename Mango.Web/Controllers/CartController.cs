1f2eec76b190c3447cd146fb576acdffd5076121
KasimAsus <itskasimsache@gmail.com>
2026-02-18 10:56:01 +0530

    Build is successful. From here on Coupon service willl be used with [Authorize] enabled.

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -1,8 +1,11 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+
 using Newtonsoft.Json;
+
 using System.IdentityModel.Tokens.Jwt;
 using System.Threading.Tasks;
 
@@ -25,16 +28,62 @@ namespace Mango.Web.Controllers
             return View(await LoadCartDtoBasedOnLoggedInUser());
         }
 
+        public async Task<IActionResult> Remove(int cartDetailsId)
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?
+                                    .FirstOrDefault()?.Value;
+
+            ResponseDto? response = await _cartService.RemoveFromCartAsync(cartDetailsId);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Cart updated successfully!";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ApplyCoupon(CartDto cartDto)
+        {
+            var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?
+                                    .FirstOrDefault()?.Value;
+
+            ResponseDto? response = await _cartService.ApplyCouponAsync(cartDto);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Cart updated successfully!";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveCoupon(CartDto cartDto)
+        {
+            // TODO: Shoudl we assign like this?
+            cartDto.CartHeader.CouponCode = "";
+            ResponseDto? response = await _cartService.ApplyCouponAsync(cartDto);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Cart updated successfully!";
+                return RedirectToAction(nameof(CartIndex));
+            }
+            return View();
+        }
+
         private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
         {
             //TODO: Why this is working? How user was accessed?
             var userId = User.Claims.Where(u => u.Type == JwtRegisteredClaimNames.Sub)?
                                     .FirstOrDefault()?.Value;
 
-            ResponseDto? response =  await _cartService.GetCartByUserIdAsync(userId);    
-            
+            ResponseDto? response = await _cartService.GetCartByUserIdAsync(userId);
+
             //TODO: Bug - If user don't have any item in cart, for that user CartIndex doesn't load
-            if(response!=null && response.IsSuccess)
+            if (response != null && response.IsSuccess)
             {
                 CartDto cartDto = JsonConvert.DeserializeObject<CartDto>(Convert.ToString(response.Result));
                 return cartDto;
