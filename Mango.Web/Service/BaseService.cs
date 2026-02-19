0d6223494a25350ec882f67a842188fb8d41c438
KasimAsus <itskasimsache@gmail.com>
2026-02-19 14:13:27 +0530

    save

--- Mango.Web/Service/BaseService.cs
+++ Mango.Web/Service/BaseService.cs
@@ -59,10 +59,11 @@ namespace Mango.Web.Service
                 //Request is sent here
                 apiResponse = await client.SendAsync(message);
 
-                ///Note: CouponAPI return ResponseDTO Whereas AuthAPI returens IActionResult
-                ///API might return ResponseDto or IActionResult(or its derived child ObjectResult)
-                ///Do remember, ObjectResult = IActionResult + Status Code + ResponseDto
-                ///      (OK/BadRequest/etc.)  (Inheritance)  (int Property) (object? Property)
+                /// Note: 
+                /// CouponAPI return ResponseDTO Whereas AuthAPI returens IActionResult
+                /// API might return ResponseDto or IActionResult(or its derived child ObjectResult)
+                /// Do remember, ObjectResult = IActionResult + Status Code + ResponseDto
+                /// (OK/BadRequest/etc.)  (Inheritance)  (int Property) (object? Property)
 
                 //For POST & PUT cases messages are not handled
                 switch (apiResponse.StatusCode)
