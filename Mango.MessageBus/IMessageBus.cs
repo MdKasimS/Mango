4887d498ed767c683fd20fda6d96cb564a665248
KasimAsus <itskasimsache@gmail.com>
2026-02-24 16:14:48 +0530

    IMessageBus interface added

---
+++ Mango.MessageBus/IMessageBus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mango.MessageBus
+{
+    internal interface IMessageBus
+    {
+        Task PublishMessage(object message, string topic_queue_name);
+    }
+}
