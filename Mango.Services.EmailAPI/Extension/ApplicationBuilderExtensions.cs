09b131b474911e115e3811d6caa607b11148fdb3
KasimAsus <itskasimsache@gmail.com>
2026-02-28 23:10:16 +0530

    Added extension to start & stop our consumer exactly with EmailAPI process. Middleware is added. RabbitMQ consumer is working correctly. Alhamdulillah!

---
+++ Mango.Services.EmailAPI/Extension/ApplicationBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Mango.MessageBus;
+
+namespace Mango.Services.EmailAPI.Extension
+{
+    public static class ApplicationBuilderExtensions
+    {
+        private static IMessageConsumer _rabbitMqMessageConsumer { get; set; }
+
+        //TODO: In extension class, why everything is static required. What special usecase is fro static constructor?
+        public static IApplicationBuilder UseRabbitMQMessageConsumer(this IApplicationBuilder app)
+        {
+            _rabbitMqMessageConsumer = app.ApplicationServices.GetService<IMessageConsumer>();
+            var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+            hostApplicationLife.ApplicationStarted.Register(OnStart);
+            hostApplicationLife.ApplicationStopping.Register(OnStop);
+
+            return app;
+        }
+
+        private static void OnStop()
+        {
+            _rabbitMqMessageConsumer.Stop();
+        }
+
+        private static void OnStart()
+        {
+            _rabbitMqMessageConsumer.Start();
+        }
+    }
+}
