cdb0c6505c390f38e8ebbe2c70dd37ea668490ae
KasimAsus <itskasimsache@gmail.com>
2026-03-04 22:35:32 +0530

    Models for OrderAPI are ready - OrderHeader, OrderDetails. Next is add Dtos for Order models.

---
+++ Mango.Services.OrderAPI/Models/OrderHeader.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Services.OrderAPI.Models
+{
+    public class OrderHeader
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double OrderTotal { get; set; }
+
+        //TODO: As per Tutor used here Name. Try to reduce props for to Name in other services as well.
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Status { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string? StripeSessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+    }
+}
