33ee0796738195d231a8ce37b078bb22ce67fa29
KasimAsus <itskasimsache@gmail.com>
2026-03-04 23:32:40 +0530

    Mapping added successfully. Build is successful.

--- Mango.Services.OrderAPI/MappingConfig.cs
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -14,9 +14,20 @@ namespace Mango.Services.OrderAPI
 
             var mappingConfig = new MapperConfiguration(config =>
             {
+                //TODO: Inspect this why mapping is in this way
+                config.CreateMap<OrderHeaderDto, CartHeaderDto>()
+                  .ForMember(dest => dest.CartTotal, u => u.MapFrom(src => src.OrderTotal)).ReverseMap();
+
+                config.CreateMap<CartDetailsDto, OrderDetailsDto>()
+                  .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Name))
+                  .ForMember(dest => dest.Price, u => u.MapFrom(src => src.Product.Price));
+
+                config.CreateMap<OrderDetailsDto, CartDetailsDto>().ReverseMap();
+
+                //Note: Deviation from Tutor
                 config.CreateMap<OrderHeaderDto, OrderHeader>().ReverseMap();
                 config.CreateMap<OrderDetailsDto, OrderDetails>().ReverseMap();
-                //config.CreateMap<Product, ProductDto>();
+
             }, loggerFactory);
             return mappingConfig;
         }
