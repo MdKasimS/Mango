2721f5586b59151055f0ba07aff5242255499750
KasimAsus <itskasimsache@gmail.com>
2026-03-05 07:15:25 +0530

    Created OrderDetailsDto, OrderHeaderDto. Build is successful

---
+++ Mango.Web/Models/OrderHeaderDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Mango.Web.Models.Dto
+{
+    public class OrderHeaderDto
+    {
+        public int Id { get; set; }
+        public string? UserId { get; set; }
+        public string? CouponCode { get; set; }
+        public double Discount { get; set; }
+        public double OrderTotal { get; set; }
+
+        public string? Name { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Status { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string? StripeSessionId { get; set; }
+        public string? PaymentIntentId { get; set; }
+        public IEnumerable<OrderDetailsDto> OrderDetails { get; set; }
+    }
+}
