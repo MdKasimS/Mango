e39d2789c98bcc0a2e9b2126f766173e2ba4c1cc
KasimAsus <itskasimsache@gmail.com>
2026-03-05 06:48:57 +0530

    Section 12 - 130 CreateOrder action method is ready in OrderControllerAPI. Build failed. (Because -Ln.58 Entity)

--- Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
+++ Mango.Services.OrderAPI/Controllers/OrderAPIController.cs
@@ -2,9 +2,12 @@
 
 using Mango.MessageBus;
 using Mango.Services.OrderAPI.Data;
+using Mango.Services.OrderAPI.Models;
 using Mango.Services.OrderAPI.Models.Dto;
 using Mango.Services.OrderAPI.Service.IService;
+using Mango.Services.OrderAPI.Utility;
 
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -37,6 +40,35 @@ namespace Mango.Services.OrderAPI.Controllers
             _configuration = configuration;
             _response = new ResponseDto();
             _configuration = configuration;
+        }
+
+        [HttpPost("CreateOrder")]
+        [Authorize]
+        public async Task<ResponseDto> CreateOrder([FromBody] CartDto cartDto)
+        {
+            try
+            {
+                OrderHeaderDto orderHeaderDto = _mapper.Map<OrderHeaderDto>(cartDto.CartHeader);
+                orderHeaderDto.OrderTime = DateTime.Now;
+                orderHeaderDto.Status = SD.Status_Pending;
+                orderHeaderDto.OrderDetails = _mapper.Map<IEnumerable<OrderDetailsDto>>(cartDto.CartDetails);
+
+                //TODO: Checkout this what it is in detail .Entity
+                //TODO: Check whether await works here or not. Earlier it was not working or might be timing difference due to async-await
+                OrderHeader orderCreated = await _db.OrderHeaders.AddAsync(_mapper.Map<OrderHeader>(orderHeaderDto)).Entity;
+                await _db.SaveChangesAsync();
+
+                orderHeaderDto.Id = orderCreated.Id;
+
+                _response.Result = orderHeaderDto;
+            }
+            catch(Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
         }
     }
 }
