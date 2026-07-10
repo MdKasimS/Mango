7f67fa2fd7e00b5c716ad35eeb71fd348b6db01d
KasimAsus <itskasimsache@gmail.com>
2026-07-10 21:20:25 +0530

    Started with Stripe checkout. Added StriepReqeustDto in Web

--- Mango.Web/Models/StripeRequestDto.cs
+++ Mango.Web/Models/StripeRequestDto.cs
@@ -1,6 +1,13 @@
-﻿namespace Mango.Web.Models
+﻿using Mango.Web.Models.Dto;
+
+namespace Mango.Web.Models
 {
     public class StripeRequestDto
     {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSessionId { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDto OrderHeader { get; set; }
     }
 }
