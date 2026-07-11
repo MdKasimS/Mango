367dd47f1dc752ccd546430cfbc753c248d73ae2
KasimAsus <itskasimsache@gmail.com>
2026-07-12 02:21:57 +0530

    A generalised Dto for payment request is added. Later we decide how to handle stripe, upi, cards, etc. Since this is only for simulation purposes.

---
+++ Mango.Web/Models/PaymentRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models.Dto;
+
+namespace Mango.Web.Models
+{
+    public class PaymentRequestDto
+    {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSessionId { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDto OrderHeader { get; set; }
+    }
+}
